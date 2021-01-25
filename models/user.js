9b2544c8f085332eae56f1ca1961c3ad3d3b093d
Your Name <you@example.com>
2021-01-25 11:43:36 -0800

    Added hooks, password hashing, and hashed password verification to user.js model.

--- models/user.js
+++ models/user.js
@@ -25,11 +25,20 @@ module.exports = (sequelize, DataTypes) => {
       validate: {
         isEmail: {
           args: true,
-          msg: 'Please enter a valid email address format.'
+          msg: "Please enter a valid email address format."
         }
       }
     },
-    password: DataTypes.STRING,
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: {
+          args: [8, 99],
+          msg: "Password must be between 8 and 99 characters long."
+        }
+      }
+    },
     UID: DataTypes.INTEGER,
     forumId: DataTypes.INTEGER
   }, {
@@ -37,16 +46,24 @@ module.exports = (sequelize, DataTypes) => {
     modelName: 'user',
   });
 
-// Convert email address to all lowercase to validate uniqueness, and also before we store it. 
-user.addHook('beforeValidate', (pendingUser, options) => {
-  pendingUser.email = pendingUser.email.toLowerCase()
-})
+  // Convert email address to all lowercase to validate uniqueness, and also before we store it. 
+  user.addHook('beforeValidate', (pendingUser, options) => {
+    pendingUser.email = pendingUser.email.toLowerCase()
+  })
 
-// Hashing passwords after validating if they meet the minimum/maximum lengths.
-user.addHook('beforeCreate', (pendingUser, options) => {
-  
-})
+  // Hashing passwords after validating if they meet the minimum/maximum lengths.
+  user.addHook('beforeCreate', (pendingUser, options) => {
+    let hashedPassword = bcrypt.hashSync(pendingUser.password, 10) // Adding 10 salt rounds.
+    pendingUser.password = hashedPassword
+  })
 
+  // 'prototype' means we add this function to the user class instead of a specific user.
+  // Putting async in front of function says that somewhere in our fxn, we're going to tell it to pause, or 'await' for results of the following code before continuing
+  user.prototype.validPassword = async function(passwordInput) { // Have to use function declaration instead of arrow function, because otherwise the 'this' binding works differently.
+    let match = await bcrypt.compare(passwordInput, this.password) // 'this' refers to user in user.prototype, and is the hashed version since user.password was reassigned.
+    console.log(`🐶??? Was the password a match? True or false: ${match}`)
+    return match
+  }
 
   return user;
 };
