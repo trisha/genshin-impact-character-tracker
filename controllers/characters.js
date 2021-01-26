14ff045b3c625f00f4781ad4f62ac82ef4d161cd
Your Name <you@example.com>
2021-01-26 14:51:29 -0800

    Added bootstrap-select and added a multiselect dropdown for adding new characters. Added color coding and decided against it.

--- controllers/characters.js
+++ controllers/characters.js
@@ -4,13 +4,21 @@ const db = require('../models')
 const passport = require('../config/passportConfig.js') // Import module we coded.
 
 router.get('/', (req, res) => {
-    // let backName = req.user.name + ' Pan'
+    // let backName = req.user.name + ' Pan' (instead of res.locals.currentUser.name)
     res.render('characters/myCharacters.ejs')
 })
 
-router.put('/new', (req, res) => {
-    console.log(`🐻 ${req.body.value}`)
-    console.log(`🐻 ${req.body.character}`)
+router.get('/all', (req, res) => {
+    res.render('characters/allCharacters.ejs')
+})
+
+router.get('/partial', (req, res) => {
+    res.render('partials/view.ejs')
+})
+
+router.post('/new', (req, res) => {
+    let characters = req.body.characters
+    console.log('🐹Selected characters: ', characters)
     res.redirect('/characters')
 })
 
