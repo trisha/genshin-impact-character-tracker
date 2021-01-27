94823536a132bfec8802a939a4b9a316f5db819d
Your Name <you@example.com>
2021-01-27 14:33:08 -0800

    Added ability to add goals from /dashboard to the goals SQL table.

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -4,7 +4,7 @@ const db = require('../models')
 const passport = require('../config/passportConfig.js') // Import module we coded.
 const isLoggedIn = require('../middleware/isLoggedIn.js')
 
-
+// Show all character goals on dashboard.
 router.get('/', isLoggedIn, (req, res) => {
     db.myCharacter.findAll({
         where: {
@@ -14,14 +14,35 @@ router.get('/', isLoggedIn, (req, res) => {
             ['name', 'ASC']
         ]
     }).then(myCharacters => {
+        myCharacters.forEach((myChar, index) => {
+            // Grab all the goals as well.
+            let goals = []
+            db.goal.findAll({
+                where: {
+                    myCharacterId: myChar.id
+                }
+            }).then(foundGoals => {
+                myChar.goals = foundGoals // We add this to our JavaScript object but don't save it to our SQL database.
+            })
+        })
         res.render('dashboard/dashboardView.ejs', {myCharacters: myCharacters})
     })
 })
 
+// Select a character and add a new goal. 
 router.post('/newgoal', (req, res) => {
-    // console.log(req.body.character, req.body.goal)
-    console.log("We've hit our POST route!")
-    res.redirect('/dashboard')
+    // console.log('🐣 character: ', req.body.myCharId)
+    // console.log('🐣 goal: ', req.body.goal)
+    db.goal.create({
+        myCharacterId: req.body.myCharId,
+        li: req.body.goal // li as in List Item.
+    }).then(goal => {
+        db.myCharacter.findOne({where: {id: req.body.myCharId}})
+        .then(foundChar => {
+            foundChar.addGoal(goal)
+        })
+    }).catch(err => { console.log(err) }) // If there's an error creating a goal.
+    res.redirect('/dashboard') 
 })
 
 module.exports = router
